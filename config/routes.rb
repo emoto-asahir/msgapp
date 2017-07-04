8775eb1ddb9f3c6a094b9195adf99ced25ece776
lghpws <lghpws@gmail.com>
2017-07-04 04:16:07 +0000

    2nd commit

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   get 'logout' , to: 'aaa#destroy'
   get 'test' , to: 'aaa#test'
   get '/custom_token/:id',to: 'firebase_tokens#show_token'
-  get '/roo/aaa',to: 'chat_rooms#show_a_room'
   get '/room/:receiver_id/:sender_id',to: 'chat_rooms#show_the_room'
   post 'friendships' , to: 'friendships#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
